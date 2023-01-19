e14e2a9a54c35d382c304ebc5490af69ddcf6844
Jaavii43 <jatoro_2001@yahoo.com.ar>
2023-01-19 17:06:31 -0300

    probandohabilidades

--- src/main/java/com/argprograma/Portfolio/Controller/CHabilidades.java
+++ src/main/java/com/argprograma/Portfolio/Controller/CHabilidades.java
@@ -6,7 +6,6 @@ import com.argprograma.Portfolio.entity.Habilidades;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,41 +17,41 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-// @Controller
-// @RequestMapping("habilidad")
-// @CrossOrigin(origins = "http://localhost:4200")
+
+@RequestMapping("habilidad")
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class CHabilidades {
     
     @Autowired
     private ISHabilidades habServ;
 
-    @GetMapping("habilidades/ver")
+    @GetMapping("ver")
     public List<Habilidades>getHabilidades()
         {
         return habServ.getSkins();
         }
-    @GetMapping("habilidad/traer")
+    @GetMapping("traer")
     public Habilidades findhabilidades()
         {
         return habServ.findSkin((long)1);
         }
     
     
-    @PostMapping("habilidad/new")
+    @PostMapping("new")
     public String agregarHabilidades(@RequestBody Habilidades hab)
         {
         habServ.saveSkins(hab);
         return "La habilidad fue creada correctamente";
         }
 
-    @DeleteMapping("habilidad/delete/{id}")
+    @DeleteMapping("delete/{id}")
     public String deleteHabilidad(@PathVariable Long id)
         {
         habServ.deleteSkin(id);
         return "La persona fue borrada correctamente";
         }
-    @PutMapping ("habilidad/editar/{id}")
+    @PutMapping ("editar/{id}")
         public Habilidades editHabilidades(@PathVariable Long id,
                             @RequestParam ("area") String nuevaArea,
                             @RequestParam ("porcentaje") int nuevoPorcentaje)
