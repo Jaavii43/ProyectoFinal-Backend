c31430a9224c1c5677d2dd5e9e293f51aab30b6d
Jaavii43 <109696723+Jaavii43@users.noreply.github.com>
2023-03-01 22:52:16 -0300

    seguimosagregando

--- src/main/java/com/argprograma/Portfolio/Controller/CProyectos.java
+++ src/main/java/com/argprograma/Portfolio/Controller/CProyectos.java
@@ -5,39 +5,49 @@ import com.argprograma.Portfolio.Service.ISProyectos;
 import com.argprograma.Portfolio.entity.Proyectos;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+
+@RequestMapping("proyectos")
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class CProyectos {
     
     @Autowired
     private ISProyectos proyServ;
 
-    @GetMapping("ver/proyectos")
+    @GetMapping("ver")
     public List<Proyectos>getProyecto(){
         return proyServ.getProyecto();
     }
     
-    @PostMapping("new/proyectos")
+    @GetMapping("traer")
+    public Proyectos findProyecto(){
+        return proyServ.findProyecto((long)1);
+    }
+    
+    @PostMapping("new")
     public String agregarProyecto(@RequestBody Proyectos proy){
         proyServ.saveProyecto(proy);
         return "La persona fue creada correctamente";
    }
 
-    @DeleteMapping("delete/proyectos/{id}")
+    @DeleteMapping("delete/{id}")
     public String deleteEmpresa(@PathVariable Long id)
     {
         proyServ.deleteProyecto(id);
         return "El proyecto fue borrado correctamente";
     }
-    @PutMapping ("editar/proyectos/{id}")
+    @PutMapping ("editar/{id}")
         public Proyectos editProyectos(@PathVariable Long id,
                             @RequestParam ("tipo") String nuevoTipo,
                             @RequestParam ("sector") String nuevoSector,
